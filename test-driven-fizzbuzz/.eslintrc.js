ba472d3d6c73074f20a3cc3803750305b3fa99e7
Deiziane Silva <deiziane_ns@hotmail.com>
2020-09-19 15:07:34 -0300

    :wrench: adds jest ESLint setup

--- test-driven-fizzbuzz/.eslintrc.js
+++ test-driven-fizzbuzz/.eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ["eslint:recommended"],
+  plugins: ["react"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: { ecmaVersion: 6 },
   env: { node: true },
+  settings: {
+    react: {
+      version: "16.4.2",
+    },
+  },
 };
