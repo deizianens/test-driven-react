a06a39326448d80c7b1d5284836aa3eefc292fc0
Deiziane Silva <deiziane_ns@hotmail.com>
2020-09-20 16:38:33 -0300

    :wrench: initial ESlint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
   plugins: ["react"],
   extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: { ecmaVersion: 6 },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   env: { node: true },
+  rules: {
+    quotes: ["error", "single", { avoidEscape: true }],
+    "comma-dangle": ["error", "always-multiline"],
+  },
   settings: {
     react: {
       version: "16.4.2",
---
+++ src/tests/.eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = { plugins: ["jest"], extends: ["plugin:jest/recommended"] };
